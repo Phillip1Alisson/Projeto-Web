03bdd03e14e3e42eaeb754e9a59df9d7151609f6
Phillip <phill.volpi@gmail.com>
2017-03-22 12:39:34 -0300

    Update, modify variable name

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function somar(a, b){
 
 
 document.getElementById("btn-calcular").onclick = function(){
-  var valueA = document.getElementById("value-a").value;
+  var valueDC = document.getElementById("value-a").value;
   var valueB = document.getElementById("value-b").value;
   console.log(somar(parseInt(valueA),parseInt(valueB)));
 
